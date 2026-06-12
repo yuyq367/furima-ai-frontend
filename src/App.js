122e921f5b180df59fab18f92fba5e5fbd10c563
yuyq367 <yuya.mngdev@gmail.com>
2026-06-12 16:22:35 +0900

    Extract my page component

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { auth } from "./firebase";
 import HomePage from "./pages/HomePage";
 import ProductDetailPage from "./pages/ProductDetailPage";
 import SellPage from "./pages/SellPage";
+import MyPage from "./pages/MyPage";
 
 function App() {
   const [email, setEmail] = useState("");
@@ -403,58 +404,7 @@ function App() {
           )}
 
           {isMyPage && (
-            <>
-              <hr />
-
-              <h3>自分の出品一覧</h3>
-
-              {myProducts.length === 0 ? (
-                <p>出品した商品はありません</p>
-              ) : (
-                <div>
-                  {myProducts.map((product) => (
-                    <div
-                      key={product.id}
-                      style={{
-                        border: "1px solid #ddd",
-                        padding: "12px",
-                        marginBottom: "12px",
-                        borderRadius: "8px",
-                      }}
-                    >
-                      <h4>{product.title}</h4>
-                      <p>{product.price}円</p>
-                      <p>販売状況: {product.status}</p>
-                    </div>
-                  ))}
-                </div>
-              )}
-
-              <h3>自分の購入一覧</h3>
-
-              {myPurchases.length === 0 ? (
-                <p>購入した商品はありません</p>
-              ) : (
-                <div>
-                  {myPurchases.map((purchase) => (
-                    <div
-                      key={purchase.purchase_id}
-                      style={{
-                        border: "1px solid #ddd",
-                        padding: "12px",
-                        marginBottom: "12px",
-                        borderRadius: "8px",
-                      }}
-                    >
-                      <h4>{purchase.title}</h4>
-                      <p>{purchase.price}円</p>
-                      <p>出品者: {purchase.seller_username}</p>
-                      <p>購入日時: {purchase.purchased_at}</p>
-                    </div>
-                  ))}
-                </div>
-              )}
-            </>
+            <MyPage myProducts={myProducts} myPurchases={myPurchases} />
           )}
         </div>
       ) : (
